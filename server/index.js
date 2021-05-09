55165d2b35329c454bc2e83d9649712032f6eb39
brend3n <brenden@knights.ucf.edu>
2021-05-09 12:00:39 -0400

    Fixed index.js

--- server/index.js
+++ server/index.js
@@ -2,15 +2,24 @@ const express = require('express')
 
 const app = express()
 
-const python = require('python-shell')
+let {PythonShell} = require('python-shell')
 
 const port = 3096
+
+
+/*
+ * Example run Python script:
+ *
+ *  PythonShell.run('my_script.py', null, function (err) {
+ *   if (err) throw err;
+ *     console.log('finished');
+ *     });
+ *
+ */
 
 app.get('/', (req,res) => {
     res.send("Hello!");
 })
-
-
 
 app.listen(port, () => {
     console.log(`Listening at: http://localhost:${port}`)
