7fb255fd63df75349cfaafe8915d1907e4b665d6
brend3n <brenden@knights.ucf.edu>
2021-05-09 11:56:11 -0400

    Added node server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+const port = 3096
+
+app.get('/', (req,res) => {
+    res.send("Hello!");
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Listening at: http://localhost:${port}`)
+})
